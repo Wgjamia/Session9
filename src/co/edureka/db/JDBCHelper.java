b18848c05236bf31fec7b4219499c07b88de2820
Wesam Ali Algjamia <wgjamia@gmail.com>
2018-10-30 19:59:47 +0200

    Complete connection DB

--- src/co/edureka/db/JDBCHelper.java
+++ src/co/edureka/db/JDBCHelper.java
@@ -1,6 +1,14 @@
 package co.edureka.db;
 
+import co.edureka.model.Student;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.Statement;
+
 public class JDBCHelper {
+     Connection con;
+     Statement stmt;
     public JDBCHelper(){
         try {
             Class.forName("com.mysql.jdbc.Driver");
@@ -8,6 +16,43 @@ public class JDBCHelper {
 
         }catch (Exception e){
             System.out.println("Some Exception: "+e);
+        }
+    }
+
+    public void CreateConnection(){
+        try {
+            String user = "wgjamia";
+            String password = "Student";
+            String url = "jdbc:mysql://localhost/edureka";
+            con = DriverManager.getConnection(url,user,password);
+            System.out.println("--Connection Created--");
+
+
+        }catch (Exception e){
+            System.out.println("Some Exception: "+e);
+        }
+    }
+    public void insertStudent(Student student){
+        try {
+                String sql = "insert into student values(null,'"+student.getName()+"','"+student.getEmail()+"',"+student.getAge()+"" +
+                        ",'"+student.getAddress()+"')";
+                stmt = con.createStatement();
+                int i = stmt.executeUpdate(sql);
+            System.out.println("Student inserted:"+i);
+
+
+        }catch (Exception e){
+            System.out.println("Some Exception:"+e);
+        }
+    }
+    public void closeConnection(){
+        try {
+            stmt.close();
+            con.close();
+            System.out.println("--Connection Close--");
+
+        }catch (Exception e){
+            System.out.println("Some Exception:"+e);
         }
     }
 
--- src/co/edureka/main/Client.java
+++ src/co/edureka/main/Client.java
@@ -6,8 +6,15 @@ import co.edureka.model.Student;
 public class Client {
     public static void main(String[] args){
         Student sRef = new Student("Wesam Aljamia","W.gjamia@ltt.ly",40,"HilAlndlous");
+        Student sRef1 = new Student("Abourhman Wesam","abdourhman@ltt.ly",10,"HilAlndlous");
 
         JDBCHelper helper = new JDBCHelper();
+
+        helper.CreateConnection();
+
+        helper.insertStudent(sRef1);
+
+        helper.closeConnection();
 
 
     }
